3aa55cbbd4f52b0e7eea1a4e89499e8dec5c0df7
Alfonso Pruneda-Suarez <alfonso.pru@outlook.com>
2019-09-10 09:23:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -31,7 +31,13 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
-  
+  source_array.each do |i|
+    if i
+      return true
+    else
+      return false
+    end
+  end
 end
   
   
